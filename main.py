ca5cb9aa99092ca28c2406fc04117559ba8477f7
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-18 22:27:50 +0400

    Refinements

--- main.py
+++ main.py
@@ -33,21 +33,15 @@ canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "b
 canvas.grid(row=1, column=1)
 
 # Add Start Button
-start_button = Button(text="Start", font=(FONT_NAME, 14, "bold"))
+start_button = Button(text="Start", font=(FONT_NAME, 14, "bold"), highlightthickness=0)
 start_button.grid(row=2, column=0)
 
-# Add Checkmark Label
-checkmark_label = Label(text="✅", font=(14))
-checkmark_label.grid(row=2, column=1)
-
 # Add Reset Button
-reset_button = Button(text="Reset", font=(FONT_NAME, 14, "bold"))
+reset_button = Button(text="Reset", font=(FONT_NAME, 14, "bold"), highlightthickness=0)
 reset_button.grid(row=2, column=2)
 
-
-
-
-
-
+# Add Checkmark Label
+checkmark_label = Label(text="✅", fg=GREEN, bg=YELLOW, font=(14))
+checkmark_label.grid(row=3, column=1)
 
 window.mainloop()
