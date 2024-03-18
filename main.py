d47b999bfc8d405da23bfb7b24ad59ef26580a7a
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-18 22:09:46 +0400

    Add Canvas

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ window = Tk()
 window.title("Taha Pomodoro")
 window.config(padx=100, pady=50)
 
+# Add Image using Canvas
 canvas = Canvas(width=200, height=224)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(102, 112, image=tomato_img)
