7059a7d6a690d95c1806c2ea362a50d96bbeee15
Taha Ahmed Abdelazim Hussein <71207531+TahaThinks@users.noreply.github.com>
2024-03-18 22:13:52 +0400

    Add Canvas

--- main.py
+++ main.py
@@ -21,11 +21,15 @@ window = Tk()
 window.title("Taha Pomodoro")
 window.config(padx=100, pady=50)
 
+# Add Timer Label
+timer_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 45, "bold"))
+timer_label.pack()
+
 # Add Image using Canvas
-canvas = Canvas(width=200, height=224)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(102, 112, image=tomato_img)
-canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.pack()
 
 
